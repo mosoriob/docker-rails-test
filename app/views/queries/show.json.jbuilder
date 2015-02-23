1825fcce6a96d522cabad13b29ad4fda46a8b9c6
Maximiliano <mosorio@linets.cl>
2015-02-23 13:07:58 -0300

    query

---
+++ app/views/queries/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @query, :id, :name, :title, :content, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :queries
+
   resources :shops
 
   resources :clients
